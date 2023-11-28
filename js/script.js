44a1facf66a7d2253726cda621917443dda43828
JhonatanDczel <jariasq@unsa.edu.pe>
2023-11-28 16:22:02 -0500

    Funcionalidad de editar

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ const cancelarButton = addBookForm.querySelector(".emergente.cancelar");
 const cardContainer = document.querySelector("main");
 const cards = document.querySelectorAll(".card");
 const cardTemplate = document.querySelector(".card-template").cloneNode(true);
+const editCardContainer = document.querySelector('.ventana-emergente.edit-card');
+
 
 aceptarButton.addEventListener('click', addBookToLibrary);
 
@@ -58,6 +60,8 @@ function actualizarCards() {
     newCard.querySelector(".card-autor").textContent = book.autor;
     newCard.querySelector(".card-pages").textContent = book.pages;
     newCard.querySelector(".card-read").textContent = book.pages ? "Read" : "Not yet";
+    newCard.dataset.index = myLibrary.indexOf(book);
+
 
     let deleteButton = newCard.querySelector(".delete-button");
     deleteButton.addEventListener('click', () => {
@@ -66,9 +70,49 @@ function actualizarCards() {
       myLibrary.splice(index, 1);
     });
 
+    let index = myLibrary.indexOf(book);
+    let editButton = newCard.querySelector(".edit-button");
+    editButton.addEventListener('click', () => { editCard(newCard, index) });
+
     newCard.classList.remove("card-template");
     newCard.classList.add("card");
 
     cardContainer.appendChild(newCard);
+  });
+}
+
+let indexEditingNow;
+const editAceptar = document.querySelector(".emergente.aceptar.edit");
+const editCancel = document.querySelector(".emergente.cancelar.edit");
+editCancel.addEventListener("click", () => {
+  cardContainer.classList.remove("blur");
+  ventanaEmergente.dataset.visible = "false";
+});
+
+
+function clearForm() {
+
+}
+
+
+function editCard(card, index) {
+  indexEditingNow = index;
+  editCardContainer.dataset.visible = "true";
+  cardContainer.classList.add("blur");
+  console.log(card);
+  editAceptar.addEventListener('click', () => {
+    editCardContainer.dataset.visible = "false";
+    cardContainer.classList.remove("blur");
+
+    const title = document.getElementById("book-title-edited").value;
+    const autor = document.getElementById("book-autor-edited").value;
+    const pages = document.getElementById("book-pages-edited").value;
+    const read = document.getElementById("book-read-edited").checked;
+
+    card.querySelector(".card-title").textContent = myLibrary[index].title = title;
+    card.querySelector(".card-autor").textContent = myLibrary[index].autor = autor;
+    card.querySelector(".card-pages").textContent = myLibrary[index].pages = pages;
+    card.querySelector(".card-read").textContent = myLibrary[index].read = read;
+
   });
 }
