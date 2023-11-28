26c440269290473f08d024094fb5a18bfb646d21
JhonatanDczel <jariasq@unsa.edu.pe>
2023-11-28 12:11:45 -0500

    Funciones basicas

--- js/generate-cards.js
+++
@@ -1,7 +0,0 @@
-const card = document.querySelector(".card");
-
-for (let i = 0; i < 10; i++) {
-  let newCard = card.cloneNode(true);
-
-  document.querySelector('main').appendChild(newCard);
-}
--- js/script.js
+++ js/script.js
@@ -4,8 +4,21 @@ const addBookForm = document.querySelector('.ventana-emergente > form');
 const newBookButton = document.getElementById("new-button");
 const aceptarButton = addBookForm.querySelector(".emergente.aceptar");
 const cancelarButton = addBookForm.querySelector(".emergente.cancelar");
+const cardContainer = document.querySelector("main");
+const cards = document.querySelectorAll(".card");
+const cardTemplate = document.querySelector(".card-template").cloneNode(true);
 
 aceptarButton.addEventListener('click', addBookToLibrary);
+
+cards.forEach(card => {
+  let deleteButton = card.querySelector(".delete-button");
+  deleteButton.addEventListener('click', () => { card.remove() });
+});
+
+cancelarButton.addEventListener('click', () => {
+  cardContainer.classList.remove("blur");
+  ventanaEmergente.dataset.visible = "false";
+});
 
 function Book(title, autor, pages, read) {
   this.title = title;
@@ -23,11 +36,31 @@ function addBookToLibrary() {
   const book = new Book(title, autor, pages, read);
 
   myLibrary.push(book);
+
+  cardContainer.classList.remove("blur");
+  ventanaEmergente.dataset.visible = "false";
+  actualizarCards();
 }
 
 newBookButton.addEventListener("click", mostrarForm);
 
 function mostrarForm() {
   ventanaEmergente.dataset.visible = "true";
-  document.querySelector("main").classList.add("blur");
+  cardContainer.classList.add("blur");
+}
+
+function actualizarCards() {
+  cardContainer.querySelectorAll(".card:not(.card-template)").forEach((e) => e.remove());
+  myLibrary.forEach((book) => {
+    const newCard = cardTemplate.cloneNode(true);
+
+    newCard.querySelector(".card-title").textContent = book.title;
+    newCard.querySelector(".card-autor").textContent = book.autor;
+    newCard.querySelector(".card-pages").textContent = book.pages;
+    newCard.querySelector(".card-read").textContent = book.pages ? "Read" : "Not yet";
+    newCard.classList.remove("card-template");
+    newCard.classList.add("card");
+
+    cardContainer.appendChild(newCard);
+  });
 }
