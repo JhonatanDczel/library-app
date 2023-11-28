0e473c8d7d79cd7d373fce342b950c91748ea926
JhonatanDczel <jariasq@unsa.edu.pe>
2023-11-28 11:32:08 -0500

    Agrega funcion addbook

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 const myLibrary = [];
+const ventanaEmergente = document.querySelector('.ventana-emergente');
+const addBookForm = document.querySelector('.ventana-emergente > form');
+const newBookButton = document.getElementById("new-button");
+const aceptarButton = addBookForm.querySelector(".emergente.aceptar");
+const cancelarButton = addBookForm.querySelector(".emergente.cancelar");
+
+aceptarButton.addEventListener('click', addBookToLibrary);
 
 function Book(title, autor, pages, read) {
   this.title = title;
@@ -8,12 +15,19 @@ function Book(title, autor, pages, read) {
 }
 
 function addBookToLibrary() {
-  const title = prompt("Titulo:");
-  const autor = prompt("Autor:");
-  const pages = prompt("Pages:");
-  const read = prompt("Read?:");
+  const title = document.getElementById("book-title").value;
+  const autor = document.getElementById("book-autor").value;
+  const pages = document.getElementById("book-pages").value;
+  const read = document.getElementById("book-read").checked;
 
   const book = new Book(title, autor, pages, read);
 
   myLibrary.push(book);
+}
+
+newBookButton.addEventListener("click", mostrarForm);
+
+function mostrarForm() {
+  ventanaEmergente.dataset.visible = "true";
+  document.querySelector("main").classList.add("blur");
 }
